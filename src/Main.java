88ad2d790e1914dce7fe42273b32f5dc5e8aef09
alexanderik79 <alexanderik79@gmail.com>
2023-06-05 17:46:12 +0200

    пункт 3 дз. зробити зміни в другій гілці та запушити

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
         System.out.println("Hello Git!");
         System.out.println("reset Hard");
         System.out.println("Execute squash commits");
+
+        System.out.println("пункт 3 дз. зробити зміни в другій гілці та запушити");
     }
 }
